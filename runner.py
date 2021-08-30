2405135ed65a43b1451c17d46b6bf8905bcb7e99
zhuziwang <2693601181@qq.com>
2021-08-30 14:43:07 +0800

    使用Yaml

--- runner.py
+++ runner.py
@@ -4,19 +4,20 @@ import pytest
 
 #启动yaml的
 
-# from ddt.params import yaml_params
-# datas=yaml_params()
-#
-# print(datas)
-# pytest.main(['-s','ddt/login.py','--alluredir','temp','-s'])
-# os.system('allure generate temp -o report --clean')
+from ddt.params import yaml_params
+datas=yaml_params()
 
+print(datas)
+pytest.main(['-s','ddt/login.py','--alluredir','temp','-s'])
+os.system('allure generate temp -o report --clean')
+#os.system('copy environment.properties report\environment.properties')
+os.system('allure open report/html')
 
 #启动excel的
-from ddt.params import excel_params
-table_sheet=excel_params()
-#print(table_sheet)
-pytest.main(['-s','ddt/login_excel.py','--alluredir','temp','-s'])
-os.system('allure generate temp -o report/html --clean')
-# os.system('copy environment.properties report\environment.properties')
-os.system('allure open report/html')
+# from ddt.params import excel_params
+# table_sheet=excel_params()
+# #print(table_sheet)
+# pytest.main(['-s','ddt/login_excel.py','--alluredir','temp','-s'])
+# os.system('allure generate temp -o report/html --clean')
+# # os.system('copy environment.properties report\environment.properties')
+# os.system('allure open report/html')
