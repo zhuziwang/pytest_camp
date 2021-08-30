eef0ad39bf6858a6fb81e51394f4a0ad224c3730
zhuziwang <2693601181@qq.com>
2021-08-30 15:10:16 +0800

    修改少传参

--- Web/webkeys.py
+++ Web/webkeys.py
@@ -78,17 +78,17 @@ class WebKey:
         ele.clear()
         ele.send_keys(str(value))
 
-    def intoiframe(self,locator=None):
+    def intoiframe(self,ele_type=None,locator=None):
         '''
         进入iframe
         :param locator: 定位器，默认xpath
         :return:
         '''
-        ele = self.xpath(locator)
+        ele = self.xpath(ele_type,locator)
         self.driver.switch_to.frame(ele)
 
 
-    def assert_results(self,locator=None,Deserved_results=None):
+    def assert_results(self,ele_type=None,locator=None,Deserved_results=None):
         self.Deserved_results = Deserved_results
-        Actual_results = self.xpath(locator).text
+        Actual_results = self.xpath(ele_type,locator).text
         return Actual_results
