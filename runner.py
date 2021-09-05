9e13683c8ce55efa442e647b5c391f86fdb407b1
e <zhuzw112233>
2021-09-05 21:21:24 +0800

    优化启动

--- runner.py
+++ runner.py
@@ -1,22 +1,11 @@
-
 import os
 import pytest
 
-#启动yaml的
-
-# from ddt.params import yaml_params
-# datas=yaml_params()
-#
-# print(datas)
-# pytest.main(['-s','ddt/login.py','--alluredir','temp','-s'])
-# os.system('allure generate temp -o report --clean')
-
-
-#启动excel的
-from ddt.params import excel_params
-table_sheet=excel_params()
-#print(table_sheet)
+#启动web
 pytest.main(['-s','ddt/login_excel.py','--alluredir','temp','-s'])
 os.system('allure generate temp -o report/html --clean')
-# os.system('copy environment.properties report\environment.properties')
-os.system('allure open report/html')
+#os.system('allure open report/html')
+
+#启动app
+# pytest.main(['-s','ddt/app_login_excel.py','--alluredir','temp','-s'])
+# os.system('allure generate temp -o report/html --clean')
