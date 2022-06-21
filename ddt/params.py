633f51a77841e382045b454f05e2b9420e6ced2a
zhuziwang <2693601181@qq.com>
2022-06-21 16:37:11 +0800

    加入用例分组

--- ddt/params.py
+++ ddt/params.py
@@ -18,9 +18,51 @@ def app_excel_params():
 
 def demo_sql_params(table):
     from models.model_sql import PyMysql
-    sql1 = "select * from %s" % table
-    data = PyMysql.mysql(sql1)
-    CommonPublic.log(data)
+    sql1 = "select * from %s order by grop" % table
+    data = PyMysql.case(sql1)
     return data
+
+
+def demo_sql_params_group(table):
+    """
+    用例分组
+    :param table: 表名
+    :return: sum_case 分组后的用例
+    """
+    from models.model_sql import PyMysql
+    sql0 = "select count(DISTINCT grop) from %s" % table
+    sql1 = "select * from %s order by grop" % table
+    testcase_group_int = PyMysql.mysql(sql0)
+    data = PyMysql.case(sql1)
+    # CommonPublic.log("用例为%s，用例共分%s组" % (data, testcase_group_int))
+
+    table = data
+    table_case = list()
+    sum_case = list()
+    if len(table) != '':
+        testcase_group_int = testcase_group_int[0][0]
+        for i in range(0, testcase_group_int):
+            i = 0
+            table_case = []
+            sum_list = []
+            table_case.append(table[i])
+            sum_list.append(i)
+            for y in range(1, len(table)):
+                if table[i][1] == table[y][1]:
+                    sum_list.append(y)
+                    table_case.append(table[y])
+                else:
+                    pass
+            sum_case.append(table_case)
+            for v in range(len(sum_list) - 1, -1, -1):
+                del table[sum_list[v]]
+            continue
+    else:
+        pass
+    return sum_case
+
+
+if __name__ == '__main__':
+    a = demo_sql_params('app_test')
 
 
---
+++ ddt/test_sql_demo.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+import allure
+import pytest
+from Web.webkeys import WebKey
+from common.cls.public import CommonPublic
+from ddt.params import demo_sql_params_group
+
+
+@allure.feature('项目名称：用例分组')
+class TestCommerce:
+    demo_sql = demo_sql_params_group('app_test')
+    CommonPublic = CommonPublic()
+    num = 0
+    title = ''
+
+    def setup_class(self):
+        self.web = WebKey()
+        self.table = TestCommerce.demo_sql
+
+    @staticmethod
+    def setup_method():
+        table_ncols = TestCommerce.demo_sql[TestCommerce.num:TestCommerce.num + 1][0][0]
+        TestCommerce.title = table_ncols[0:][2]
+
+    @allure.story('用例:%s' % title)
+    @pytest.mark.parametrize('table', demo_sql)
+    def test_login(self, table):
+        for len_table_case in range(0, len(table)):
+            table_case_test = table[len_table_case]
+            testcases = table_case_test[4:]
+            testcases = [i for i in testcases if i != '']
+            func = getattr(self.web, testcases[0])
+            values = testcases[1:]
+            try:
+                func(*values)
+                self.CommonPublic.log(testcases)
+
+            except AssertionError as msg:
+                self.CommonPublic.log(testcases)
+                self.CommonPublic.log(msg)
+
+    @staticmethod
+    def teardown_method():
+        TestCommerce.num = TestCommerce.num + 1
