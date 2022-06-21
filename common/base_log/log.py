48463f56bdd6df9738bc44491f9c576f29a564ef
zhuziwang <2693601181@qq.com>
2022-06-21 16:22:44 +0800

    修改log日志级别

--- common/base_log/log.py
+++ common/base_log/log.py
@@ -5,52 +5,55 @@ import time
 from pathlib import Path
 from logging import handlers
 
-class Log():
 
-    def __init__(self, level="DEBUG"):
-        '''
+class Log:
+
+    def __init__(self, level="ERROR"):
+        """
         日志器对象
         :param level: 级别
         name 默认为空：root
-        '''
+        """
 
         self.log = logging.getLogger()
         self.log.setLevel(level)
 
-    def console_handle(self, level="DEBUG"):
-        '''
+    def console_handle(self, level="ERROR"):
+        """
         控制台处理器
         :param level:
         :return:
-        '''
+        """
 
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(level)
+        console_handler.setLevel("ERROR")
         # 处理器格式
         console_handler.setFormatter(self.get_formatter()[0])
         return console_handler
 
     def file_handle(self, level="DEBUG"):
-        '''文件处理器'''
+        """文件处理器"""
 
         base_dir = Path(__file__).resolve().parent.parent
         str_time = self.get_str_time()
         base_dir_path = str(base_dir)
-        file_handler = logging.FileHandler(base_dir_path+'\\base_log\\' +str_time + '-log.txt', mode='a', encoding="utf-8")
+        file_handler = logging.FileHandler(base_dir_path+'\\base_log\\' + str_time + '-log.txt', mode='a',
+                                           encoding="utf-8")
         file_handler.setLevel(level)
         # 处理器格式
         file_handler.setFormatter(self.get_formatter()[1])
         return file_handler
 
-    def get_formatter(self):
-        '''格式器'''
+    @staticmethod
+    def get_formatter():
+        """格式器"""
 
         # 定义输出格式
         fmt1 = "%(asctime)s--->%(name)s--->%(levelname)s--->%(message)s--->%(filename)s：%(lineno)d"
         fmt2 = "%(asctime)s--->%(name)s--->%(levelname)s--->%(message)s--->%(filename)s：%(lineno)d"
         console_fmt = logging.Formatter(fmt1)
         file_fmt = logging.Formatter(fmt2)
-        return console_fmt,file_fmt
+        return console_fmt, file_fmt
 
     def get_log(self):
         # 日志器添加控制台处理器
@@ -59,10 +62,8 @@ class Log():
         self.log.addHandler(self.file_handle())
         return self.log
 
-    def get_str_time(self):
+    @staticmethod
+    def get_str_time():
         str_time = time.strftime("%Y-%m-%d", time.localtime(time.time()))
         str_time = str(str_time)
         return str_time
-
-
-
