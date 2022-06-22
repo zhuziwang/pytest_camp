8d0cf4518145cb66bc3bf9e14492b13c13fa3ab5
zhuziwang <2693601181@qq.com>
2022-06-22 18:43:19 +0800

    新增手机app，mysql处理文件

---
+++ models/model_sql_app.py
@@ -0,0 +1,24 @@
+import pymysql
+
+
+class PyMysql:
+    db = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='123456',
+        database='mapping',
+        charset='utf8'
+    )
+
+    @staticmethod
+    def mysql(sql=None):
+
+        cursor = PyMysql.db.cursor()
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        cursor.close()
+
+        return data
+
+
