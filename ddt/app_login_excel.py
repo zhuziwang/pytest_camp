9b359d4a307aaff52a4ae4a3f938853a312527be
zhuziwang <2693601181@qq.com>
2022-07-14 15:14:10 +0800

    修改配置：启动文件为app

--- ddt/app_login_excel.py
+++ ddt/app_login_excel.py
@@ -1,44 +1,43 @@
 # coding=utf-8
 import allure
 import pytest
-import time
-
-import common.cls.public
-from Web.em import youjian
 from Web.appkeys import AppKey
 from common.cls.public import CommonPublic
-from ddt.params import demo_sql_params
-demo_sql = demo_sql_params('nox_browser')
-CommonPublic = CommonPublic()
+from ddt.params import demo_sql_params_group
 
 
-@allure.feature('项目名称')
+@allure.feature('项目名称：用例分组')
 class TestCommerce:
+    demo_sql = demo_sql_params_group('nox_browser')
+    CommonPublic = CommonPublic()
     num = 0
     title = ''
 
     def setup_class(self):
         self.web = AppKey()
+        self.table = TestCommerce.demo_sql
 
     @staticmethod
     def setup_method():
-        table_ncols = demo_sql[TestCommerce.num:TestCommerce.num + 1][0]
-        TestCommerce.title = table_ncols[0:][0]
+        table_ncols = TestCommerce.demo_sql[TestCommerce.num:TestCommerce.num + 1][0][0]
+        TestCommerce.title = table_ncols[0:][2]
 
     @allure.story('用例:%s' % title)
     @pytest.mark.parametrize('table', demo_sql)
     def test_login(self, table):
-        testcases = table[4:]
-        testcases = [i for i in testcases if i != '']
-        func = getattr(self.web, testcases[0])
-        values = testcases[1:]
-        try:
-            func(*values)
-            CommonPublic.log(testcases)
+        for len_table_case in range(0, len(table)):
+            table_case_test = table[len_table_case]
+            testcases = table_case_test[4:]
+            testcases = [i for i in testcases if i != '']
+            func = getattr(self.web, testcases[0])
+            values = testcases[1:]
+            try:
+                func(*values)
+                self.CommonPublic.log(testcases)
 
-        except AssertionError as msg:
-            CommonPublic.log(testcases)
-            CommonPublic.log(msg)
+            except AssertionError as msg:
+                self.CommonPublic.log(testcases)
+                self.CommonPublic.log(msg)
 
     @staticmethod
     def teardown_method():
